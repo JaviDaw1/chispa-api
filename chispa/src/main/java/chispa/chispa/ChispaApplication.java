deb29e266ea672a49271747dff306e37e01590aa
Javier Martinez <javiermarsal11@gmail.com>
2025-01-13 10:50:33 +0100

    [chs] initial commit

---
+++ chispa/src/main/java/chispa/chispa/ChispaApplication.java
@@ -0,0 +1,13 @@
+package chispa.chispa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChispaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChispaApplication.class, args);
+	}
+
+}
---
+++ chispa/src/test/java/chispa/chispa/ChispaApplicationTests.java
@@ -0,0 +1,13 @@
+package chispa.chispa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChispaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
